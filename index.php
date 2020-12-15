833420495d44761afa4695b1960f0dc9154890b2
Chris Matchett <chris@freeperiod.co.uk>
2020-12-15 15:58:04 +0000

    az-104

--- index.php
+++ index.php
@@ -1,1 +1,78 @@
-<?php print "Hello Big Drill"; ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <title>AZ-104 Study</title>
+  </head>
+  <body>
+  	<div class="container">
+    	<h1>AZ-104 Study</h1>
+
+		<div class="card mt-3">
+		  <h5 class="card-header">Microsoft Courseware</h5>
+		  <div class="card-body">
+		    <h5 class="card-title">Primary method of study</h5>
+		    <p class="card-text">
+		    	<ul>
+		    		<li>Book style learning.</li>
+		    		<li>Labs 1-11 at Learn on Demand map to Skillpipe Modules.</li>
+		    		<li>Sanbox access to Azure.</li>
+		    	</ul>
+		    
+		    	<a href="https://www.skillpipe.com/" class="btn btn-primary">Skillpipe</a>
+		    	<a href="https://alh.learnondemand.net/" class="btn btn-secondary">Learn on Demand</a>
+		    </p>
+		  </div>
+		</div>
+
+		<div class="card mt-3">
+		  <h5 class="card-header">GMETRIX</h5>
+		  <div class="card-body">
+		    <h5 class="card-title">Secondary method of study</h5>
+		    <p class="card-text">
+		    	<ul>
+		    		<li>Video content.</li>
+		    		<li>Simple Azure simulations.</li>
+		    	</ul>
+		    
+		    	<a href="https://www.gmetrix.net/" class="btn btn-primary">GMETRIX</a>
+		    </p>
+		  </div>
+		</div>
+
+		<div class="card mt-3">
+		  <h5 class="card-header">Self Study</h5>
+		  <div class="card-body">
+		    <h5 class="card-title">Secondary method of study</h5>
+		    <p class="card-text">
+		    	<ul>
+		    		<li>You can use your Met account to get an Azure Student Subscription.</li>
+		    		<li>This means you can signup to Azure without needing a credit card.</li>
+		    		<li>You will get some credit for 30 days and other services free for one year.</li>
+		    	</ul>
+		    
+		    	<a href="https://azure.com/" class="btn btn-primary">Azure</a>
+		    </p>
+		  </div>
+		</div>
+
+	</div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
